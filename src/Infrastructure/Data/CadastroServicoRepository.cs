b189f2e168acc3c7aee8fc99599236993d8305de
Vinicius Moreira <devops@atlas.eng.br>
2026-09-15 11:29:00 -0300

    fix error loading

--- src/Infrastructure/Data/CadastroServicoRepository.cs
+++ src/Infrastructure/Data/CadastroServicoRepository.cs
@@ -56,6 +56,7 @@ public sealed class CadastroServicoRepository(AtlasDbContext dbContext)
             .Include(x => x.Parcelas)
             .Include(x => x.Prestadores)
                 .ThenInclude(x => x.Prestador)
-            .Include(x => x.CodigoHistorico);
+            .Include(x => x.CodigoHistorico)
+            .AsSplitQuery();
 }
 
